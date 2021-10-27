39ae5aabeb1fd9f960edf11eeac57d0b4705fc84
shivamt002 <shivamtiwari38@gmail.com>
2021-10-27 17:07:09 +0530

    react

---
+++ test/src/components/Navbar/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem,NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
+import {FaBars} from 'react-icons/fa'
+const Navbar = ({toggle}) => {
+    return (
+       <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>Pronto</NavLogo>
+                <MobileIcon onClick={toggle}>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="about">About</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="services">Services</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="signup">SignUp</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/">Sign In</NavBtnLink>
+                </NavBtn>
+            </NavbarContainer>
+        </Nav>
+       </>
+    );
+}
+
+export default Navbar
